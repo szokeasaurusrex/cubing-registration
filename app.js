5045ae8e41f98019911bbeb1a8159caa6148c412
Daniel Szoke <szokeasaurusrex@users.noreply.github.com>
2018-10-15 19:52:38 -0400

    revert

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(process.env.dir, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json())
 
 app.use('/', indexRouter);
